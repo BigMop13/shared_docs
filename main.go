49fd59c5e8891a1cf98e683e1f1bff3c14c2aab7
BigMop13 <kamil.kurek1212@gmail.com>
2025-07-10 22:56:32 +0200

    feat: user managment

--- main.go
+++ main.go
@@ -1,33 +1,81 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-var (
-	channelId = 123
-)
+type User struct {
+	Username string
+	Email    string
+	Created  time.Time
+}
 
-func main() {
-	http.HandleFunc("/", sendRequest)
+type Document struct {
+	ID       string
+	Title    string
+	Content  string
+	Owner    string
+	Editors  []string
+	Created  time.Time
+	Modified time.Time
+}
 
-	err := http.ListenAndServe(":8080", nil)
-	fmt.Println("Server is running on port 8080")
+type SharedDocsSystem struct {
+	users     map[string]*User
+	documents map[string]*Document
+	dataDir   string
+}
 
-	if err != nil {
-		panic(err)
+func NewSharedDocsSystem() *SharedDocsSystem {
+	dataDir := "./data"
+	os.MkdirAll(dataDir, 0755)
+	os.MkdirAll(filepath.Join(dataDir, "users"), 0755)
+	os.MkdirAll(filepath.Join(dataDir, "documents"), 0755)
+
+	return &SharedDocsSystem{
+		users:     make(map[string]*User),
+		documents: make(map[string]*Document),
+		dataDir:   dataDir,
 	}
 }
 
-func sendRequest(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("test")
+func main() {
+	system := NewSharedDocsSystem()
+	reader := bufio.NewReader(os.Stdin)
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+	fmt.Println("=== Welcome to Shared Docs System ===")
+	fmt.Println("A simplified collaborative document editing system")
+	fmt.Println()
+
+	for {
+		fmt.Println("Main Menu:")
+		fmt.Println("1. Create User")
+		fmt.Println("2. Login")
+		fmt.Println("3. List Users")
+		fmt.Println("4. Exit")
+		fmt.Print("Choose an option: ")
+
+		choice, _ := reader.ReadString('\n')
+		choice = strings.TrimSpace(choice)
+
+		switch choice {
+		case "1":
+			system.createUser(reader)
+		case "2":
+			system.login(reader)
+		case "3":
+			system.listUsers()
+		case "4":
+			fmt.Println("Goodbye!")
+			return
+		default:
+			fmt.Println("Invalid option. Please try again.")
+		}
+		fmt.Println()
 	}
-
-	defer resp.Body.Close()
-
-	fmt.Println("Response Status", resp.Status)
 }
